8ada5babe67c91026db7ef40a82bd7c109257f79
citeFred <citefred@gmail.com>
2025-01-22 20:58:18 +0900

    chore : DB2 - dbconfig작성 및 의존성 주입

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { ConfigModule } from '@nestjs/config';
+import { databaseConfig } from './configs/database.config';
 
 @Module({
-  imports: [BoardsModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    BoardsModule,
+  ],
+  providers: [
+    {
+      provide: 'DATABASE_CONFIG',
+      useValue: databaseConfig,
+    },
+  ],
+  exports: ['DATABASE_CONFIG'],
 })
 export class AppModule {}
