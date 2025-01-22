61151ad24631e8ea1842561af0f9790f6f4ccbc4
citeFred <citefred@gmail.com>
2025-01-22 22:46:21 +0900

    chore : TypeORM config파일 설정 및 의존성 관계 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
-import { ConfigModule } from '@nestjs/config';
-import { databaseConfig } from './configs/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './configs/typeorm.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    BoardsModule,
-  ],
-  providers: [
-    {
-      provide: 'DATABASE_CONFIG',
-      useValue: databaseConfig,
-    },
-  ],
-  exports: ['DATABASE_CONFIG'],
+    TypeOrmModule.forRoot(typeOrmConfig),
+    BoardsModule],
 })
 export class AppModule {}
