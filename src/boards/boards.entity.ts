5ed14a0f45c1b13b0e6c744b8926890c15e6612e
citeFred <citefred@gmail.com>
2025-01-22 22:47:31 +0900

    refactor : Entity가 TypeORM이 인식 할 수 있도록 데코레이터 추가

--- src/boards/boards.entity.ts
+++ src/boards/boards.entity.ts
@@ -1,9 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { BoardStatus } from "./boards-status.enum";
 
+@Entity()
 export class Board {
+    @PrimaryGeneratedColumn()
     id: number;
+    
+    @Column()
     author: string;
+    
+    @Column()
     title: string;
+    
+    @Column()
     contents: string;
-    status: BoardStatus
+    
+    @Column()
+    status: BoardStatus;
 }
