8b360e0f33b4688cf0d4af3c29a12f841b4c6604
citeFred <citefred@gmail.com>
2025-01-29 14:15:22 +0900

    refactor : 게시글 일부 수정(공개 상태 변경) 관리자 전용 기능으로 변경

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -68,8 +68,9 @@ export class BoardsController {
         return boardResponseDto;
     }
 
-    // 특정 번호의 게시글 일부 수정
+    // 특정 번호의 게시글 일부 수정<ADMIN 기능>
     @Patch('/:id')
+    @Roles(UserRole.ADMIN)
     async updateBoardStatusById(
         @Param('id') id: number,
         @Body('status', BoardStatusValidationPipe) status: BoardStatus): Promise<void> {
