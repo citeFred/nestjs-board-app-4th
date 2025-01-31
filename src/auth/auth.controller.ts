b0b71890dd5a5fe2d5c9941b289c0ba1df293d90
citeFred <citefred@gmail.com>
2025-01-31 11:56:39 +0900

    refactor : 응답 데이터 통일을 위한 ApiResponseDto 생성과 각 컨트롤러 핸들러 변경

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Logger, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInRequestDto } from './dto/sign-in-request.dto';
 import { Response } from 'express';
+import { ApiResponseDto } from 'src/common/api-response-dto/api-response.dto';
 
 @Controller('api/auth')
 export class AuthController {
@@ -16,10 +17,12 @@ export class AuthController {
 
         const accessToken = await this.authService.signIn(signInRequestDto);
 
-        // [2] JWT를 헤더에 저장
-        res.setHeader('Authorization', accessToken);
-        
-        res.send({message: "Login Success", accessToken});
         this.logger.verbose(`User with email: ${signInRequestDto.email} issued JWT ${accessToken}`);
+
+        // [2] JWT를 헤더에 저장 후 ApiResponse를 바디에 담아서 전송
+        res.setHeader('Authorization', accessToken);
+        const response = new ApiResponseDto(true, 200, 'User logged in successfully', { accessToken });
+
+        res.send(response);
     }
 }
